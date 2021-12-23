38ec9960ab506ed42f08c1edceb73707ea4926ab
Petri Haikonen <petri.haikonen@ecxol.net>
2021-12-24 01:35:10 +0200

    Preparations for PHPStan

---
+++ checks-bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+const DBHOST = 'test';
+const DBNAME = 'test';
+const DBPASS = 'test';
+const DBUSER = 'test';
+const HASHKEY = 'will-be-removed';
+
+const SESSIONPREFIX = 'test';
+const USER_IP = '127.0.0.1';
+const PROTOCOL = 'http';
+const SITE_DOMAIN = '127.0.0.1';
+
+const ROOT_DIR = '';
+const PUB_PATH_CAT = 'test';
+const PUB_PATH = '';
+const CATEGORY = '';
+const CATEGORY_NAME = '';
+const CATEGORY_ID = 0;
+const ACTIVE_PAGE = 'test';
+const FILE_FOLDER = '';
+const DOC_DIR = '';
+const THEME = '';
