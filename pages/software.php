5bc5c6b2f320f5ac26ef36b9abe926ae24fc513c
Petri Haikonen <petri.haikonen@ecxol.net>
2024-07-24 14:41:46 +0300

    Software page updates
    - Removed Win 7 client as the OS is not supported
    - Added Media RSS as any software can use that

--- pages/software.php
+++ pages/software.php
@@ -11,19 +11,15 @@ $softwarePage->setPageTitleAddition('Software');
 
 $html = '<div id="content"><div>';
 $html .= '<h1>Software</h1>';
-$html .= '<h3>Windows 7 wallpaper changer / downloader</h3>';
-$html .= '<p style="font-size:20px;"><b>' .
-    '<a href="http://mailspeise.at/MLW/" target="_blank">Download</a></b></p>';
-$html .= '<p><b>Requires ' .
-    '<a href="http://www.oracle.com/technetwork/java/javase/downloads/index.html" target="_blank">JRE 7</a>' .
-    ' to run.</b></p>';
-$html .= '<p>The program downloads a random wallpaper in given interval and changes the desktop background to ' .
-    'downloaded wallpaper. The randoms <a href="' .
-    PUB_PATH_CAT .
-    'api/v1/random.json?limit=1&amp;search=major-colour%3Affffff" target="_blank">API</a>' .
-    ' is used for wallpaper download.</p>';
-$html .= '<p>This is a 3rd party program. For support or feedback, contact ' .
-    '<a href="https://twitter.com/relgukxilef" target="_blank">@relgukxilef</a> at Twitter.</p>';
+
+$html .= '<h3 style="margin-top:40px;">Media RSS support</h3>';
+$html .= '<p>Any software that supports Media RSS should work, just copy & paste the RSS link to the software.</p>';
+
+$html .= '<h3 style="margin-top:40px;">Variety support</h3>';
+$html .= '<p>My Little Wallpaper supports Variety through Media RSS. At the moment you can only get a RSS feed ' .
+    'for search of your choice. There is no RSS feed for favourites at the moment.</p>';
+$html .= '<p style="font-size:20px;">You can find more information about Variety <b>' .
+    '<a href="http://peterlevi.com/variety/" target="_blank">here</a></b>.</p>';
 
 $html .= '<h3 style="margin-top:40px;">Python wallpaper changer for Linux</h3>';
 $html .= '<p style="font-size:20px;"><b>' .
@@ -32,12 +28,6 @@ $html .= '<p>A simple Python script for downloading a random wallpaper from My L
     'the desktop background. Can be set to run periodically with crontab.</p>';
 $html .= '<p><strong>Note that currently the only supported desktop environments are Unity, ' .
     'Gnome and Cinnamon!</strong></p>';
-
-$html .= '<h3 style="margin-top:40px;">Variety support</h3>';
-$html .= '<p>My Little Wallpaper supports Variety through Media RSS. At the moment you can only get a RSS feed ' .
-    'for search of your choice. There is no RSS feed for favourites at the moment.</p>';
-$html .= '<p style="font-size:20px;">You can find more information about Variety <b>' .
-    '<a href="http://peterlevi.com/variety/" target="_blank">here</a></b>.</p>';
 
 $html .= '</div></div>';
 
